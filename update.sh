f1f682dafff654f73f145d0c3436a7c129331959
HAIB MOUAD <mouadhaib@gmail.com>
2022-07-11 21:07:23 +0100

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+flatpak update -y
+flatpak uninstall --unused
+neofetch
+echo "Your system is up to date you fucker"
+echo "Today is $(date)"
