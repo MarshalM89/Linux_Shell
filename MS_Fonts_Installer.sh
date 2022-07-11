cf72b67897772eeb69345b1e34cb0f24b6159c8d
HAIB MOUAD <mouadhaib@gmail.com>
2022-07-11 23:30:10 +0100

    Update MS_Fonts_Installer.sh

--- MS_Fonts_Installer.sh
+++ MS_Fonts_Installer.sh
@@ -1,2 +1,3 @@
+#Script for installing Microsoft fonts such as Times New Roman. They become available in the office apps.
 wget http://ftp.us.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.8_all.deb
 sudo dpkg -i ttf-mscorefonts-installer_3.8_all.deb
